41c2796193e4f97b10fa52c311e6d20bfafcc2ca
Célestin <flackodisk@gmail.com>
2026-08-05 09:54:17 +0100

    Update API name and operator in settings

--- setup/settings.js
+++ setup/settings.js
@@ -1,12 +1,12 @@
 module.exports = {
-  name: "Ajiro APIs",
+  name: "🌹Célestin APIs",
   description:
     "Simple and easy to use",
   key: "ajiro2005",
   header: {
     status: "Online!",
     imageSrc: [
-      "https://cdnb.artstation.com/p/assets/images/images/066/149/065/original/killer-rabbit-media-chinese-alleyway-export.gif?1692171513",
+      "https://l.facebook.com/l.php?u=https%3A%2F%2Fi.imgur.com%2FqbBgpft.gif%3Ffbclid%3DIwZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQPMjc1MjU0NjkyNTk4Mjc5AAEexgT5Lb-ZQg1fpscrg1JbpG_UxrmmGEr_0hwOuU554nig7xjQQj6ycxafbsA_aem_RetUEqaH1-EF9hO_CgS2_Q&h=AUDbNfLvWmeCGCbtaK8EUl81Epl8VMRrE2z-fYEuH8bWIAZg-nPWtecd4xevMqwKiaBGLQuPfffBIKaiSeIS65MtMFV7e348SRCedlHbp7QiqvR_qCBW8csHt_OtTyE2SI6D&s=1",
     ],
     imageSize: {
       mobile: "80%",
@@ -15,7 +15,7 @@ module.exports = {
     },
   },
   icon: "/docs/image/icon.png",
-  operator: "AjiroDesu",
+  operator: "Célestin 🌹",
   telegram: "https://t.me/+AQO22J2q6KBlNWM1",
   messenger: "https://m.me/j/Aba0mwuz9PTv7bP9/",
   github: "https://github.com/ajirodesu"
